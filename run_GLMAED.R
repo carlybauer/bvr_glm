0e3f4dd390b55845b2a651c1a9487a358a4aacb1
carlybauer <carlyeb@vt.edu>
2024-10-09 12:08:26 -0400

    new stuff

--- run_GLMAED.R
+++ run_GLMAED.R
@@ -21,14 +21,14 @@ names(nc$var) # these are all the output variables from GLM-AED
 names(nc$dim) # these are all the different dimension names of the netcdf
 
 # pull temperature (as an example)
-temp <- ncdf4::ncvar_get(nc, var = "temp")
-dim(temp) # get dimensions of temperature data
+PHY_green_IN <- ncdf4::ncvar_get(nc, var = "PHY_green_IN")
+dim(PHY_green_IN) # get dimensions of temperature data
 # [1] 500 4984
 # 500 is the number of layers (depths) in the model
 # 4984 is the number of time points
 
 # wrangle temp
-temp_df <- data.frame(t(temp))
+PHY_green_IN_df <- data.frame(t(PHY_green_IN))
 
 # get list of output times
 start <- as.POSIXct('2015-07-07 00:00:00')
@@ -37,10 +37,10 @@ end <- as.POSIXct('2022-05-03 00:00:00')
 times <- data.frame(seq(from=start, by=interval*60, to=end)[-1])
 
 # join temp_df and times
-temp_df2 <- bind_cols(times, temp_df)
-colnames(temp_df2)[1] <- "datetime"
-temp_df3 <- temp_df2 %>%
-  pivot_longer(X1:X500, names_to = "layer", values_to = "temp")
+PHY_green_IN_df2 <- bind_cols(times, PHY_green_IN_df)
+colnames(PHY_green_IN_df2)[1] <- "datetime"
+PHY_green_IN_df3 <- PHY_green_IN_df2 %>%
+  pivot_longer(X1:X500, names_to = "layer", values_to = "PHY_green_IN")
 
 # get heights
 heights <- ncdf4::ncvar_get(nc, var = "H")
@@ -51,7 +51,7 @@ heights_df3 <- heights_df2 %>%
   pivot_longer(X1:X500, names_to = "layer", values_to = "height")
 
 # join all together
-model_out <- left_join(heights_df3, temp_df3, by = c("datetime","layer"))  %>%
+green_out <- left_join(heights_df3, PHY_green_IN_df3, by = c("datetime","layer"))  %>%
   select(-layer)
 
 # select a particular depth
@@ -59,12 +59,27 @@ focal_depth <- 1.6
 max_lake_depth <- 9.3
 focal_height <- max_lake_depth - focal_depth
 
-one_depth_out <- model_out %>%
+green_depth_out <- green_out %>%
   group_by(datetime) %>%
   slice(which.min(abs(height - focal_height)))
 
 # plot temperature at one depth
-ggplot(data = one_depth_out, aes(x = datetime, y = temp))+
+ggplot(data = green_depth_out, aes(x = datetime, y = PHY_green_IN))+
   geom_line()+
-  ggtitle("Depth ~ 1.6 m")+
+  ggtitle("Depth ~ 1.6 m") +
   theme_bw()
+
+# combines parameters interested in to 1 dataframe
+baseparameters <- left_join(DOC_depth_out, Nit_depth_out, by = c("datetime", "height")) %>% 
+  left_join(Oxy_depth_out, by = c("datetime", "height")) %>% 
+  left_join(PHS_depth_out, by = c("datetime", "height"))
+
+# Convert datetime to character format
+baseparameters <- baseparameters %>%
+  mutate (year = year(datetime)) %>% 
+  filter (year == "2017") %>% 
+  mutate (datetime = format(datetime, "%Y-%m-%d %H:%M:%S")) %>% 
+  select(datetime, OGM_doc, NIT_nit, PHS_frp, OXY_oxy)
+
+# saves dataframe as csv, need to change name
+write_csv(baseparameters, file = "baseparameters.csv")
